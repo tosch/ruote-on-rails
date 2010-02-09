09f657fbf2388093268dadc0ae3c22ac020bbabc
Torsten Schönebaum <torsten.schoenebaum@planquadrat-software.de>
2010-02-09 17:31:44 +0100

    update to Rails 3 beta

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'e38f10ad37426acb42e2c386b3d9c0a61b06b75d349daf6f98377b4afb4dbf4742d2db5bfe76134fc42bd9767d2860abcf4eb3a4a7b1093edcd9a19d17d5dda3'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,8 +5,8 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_ruote-on-rails_session',
-  :secret      => '6cb5c20383d83c430e8e211335a03bba9a56976aab39afad989bc28da0a1a2847c6445f588d21aae163781da9ac20481f436eea16df90b9d3cc04242098bceec'
+  :key    => '_ruote-on-rails_session',
+  :secret => '168d27500f4df5397f9ad60753f3284b8275e10dde0f7c7fb68610cc58dfe60614a525482ec3d040fd53c7e53ee9a0bd084620c10eed494dd2605253ac256bdc'
 }
 
 # Use the database for sessions instead of the cookie-based default,
