20f687bb45bc7f1917fa39d012e9371444e88f3c
Torsten Schönebaum <torsten.schoenebaum@planquadrat-software.de>
2010-09-21 11:36:42 +0200

    update to rails 3.0.0

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,4 @@ RuoteOnRails::Application.config.session_store :cookie_store, :key => '_ruote-on
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# Config::Application.config.session_store :active_record_store
+# RuoteOnRails::Application.config.session_store :active_record_store
